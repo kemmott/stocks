e900a82b7fc09e3b871868259f900bcd7654ab8e
Kevan Emmott <kevan.emmott@capitalone.com>
2013-10-09 14:05:02 -0400

    adding stock price

--- web.rb
+++ web.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
 	"Hello, Everybody!"
+end
+
+get '/cof' do
+    resp = RestClient.get("http://www.google.com/ig/api?stock=COF&stock=AAPL&stock=GOOG")
+    doc = Nokogiri::XML(resp)
+    price = "#{doc.css('last')[1].attributes['data'].value}"
+    cof_price = {:cof=>price}
+    JSON.pretty_generate(cof_price)
 end
